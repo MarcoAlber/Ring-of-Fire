9ad414ff53422d75af1c0c8339c687c0ac60811f
Marco Alber <m.alber@onlinehome.de>
2023-07-10 21:59:25 +0200

    fixed responsive animation problem

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   firebase: {
     projectId: 'ring-of-fire-4020b',
-    appId: '1:484735778135:web:76cd3f141fe14d2645d212',
+    appId: '1:484735778135:web:cad0d939d0f24ca645d212',
+    databaseURL: 'https://ring-of-fire-4020b-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'ring-of-fire-4020b.appspot.com',
     apiKey: 'AIzaSyDYiWyaT8Q-D1fPKu9Gt-ooLqc2ZxB3vl8',
     authDomain: 'ring-of-fire-4020b.firebaseapp.com',
