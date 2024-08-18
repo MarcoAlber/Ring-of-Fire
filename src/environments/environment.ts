2ebb569dfa2ce2a2ab296e0ed7a20a8ed46c11fe
Marco Alber <m.alber@onlinehome.de>
2024-08-18 16:35:24 +0200

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   firebase: {
     projectId: 'ring-of-fire-4020b',
-    appId: '1:484735778135:web:6406d582f00838ad45d212',
+    appId: '1:484735778135:web:658b1b3c65f97bd145d212',
     databaseURL: 'https://ring-of-fire-4020b-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'ring-of-fire-4020b.appspot.com',
     apiKey: 'AIzaSyDYiWyaT8Q-D1fPKu9Gt-ooLqc2ZxB3vl8',
